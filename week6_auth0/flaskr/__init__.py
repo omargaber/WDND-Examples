c0746374a4b39f9e7e6c0e88da4feff1b355e6c0
Omar Gaber <omargaber956@gmail.com>
2020-11-14 10:05:42 +0200

    Updated auth0 exercise.

--- week6_auth0/flaskr/__init__.py
+++ week6_auth0/flaskr/__init__.py
@@ -9,6 +9,7 @@ from urllib.request import urlopen
 import json
 
 from models import setup_db, Account
+from validation import *
 
 
 def create_app(test_config=None):
@@ -23,76 +24,6 @@ def create_app(test_config=None):
     #     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     #     return response
     # __________________________________________________AUTH0__________________________________________________
-
-    AUTH0_DOMAIN = 'DOMAIN_HERE'
-    ALGORITHMS = ['RS256']
-    API_AUDIENCE = 'API_AUDIENCE'
-
-    class AuthError(Exception):
-        def __init__(self, error, status_code):
-            self.error = error
-            self.status_code = status_code
-
-    def verify_decode_jwt(token):
-        # GET THE PUBLIC KEY FROM AUTH0
-        jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
-        jwks = json.loads(jsonurl.read())
-        # GET THE DATA IN THE HEADER
-        unverified_header = jwt.get_unverified_header(token)
-
-        # CHOOSE OUR KEY
-        rsa_key = {}
-        if 'kid' not in unverified_header:
-            raise AuthError({
-                'code': 'invalid_header',
-                'description': 'Authorization malformed.'
-            }, 401)
-
-        for key in jwks['keys']:
-            if key['kid'] == unverified_header['kid']:
-                rsa_key = {
-                    'kty': key['kty'],
-                    'kid': key['kid'],
-                    'use': key['use'],
-                    'n': key['n'],
-                    'e': key['e']
-                }
-
-        # Finally, verify!!!
-        if rsa_key:
-            try:
-                # USE THE KEY TO VALIDATE THE JWT
-                payload = jwt.decode(
-                    token,
-                    rsa_key,
-                    algorithms=ALGORITHMS,
-                    audience=API_AUDIENCE,
-                    issuer='https://' + AUTH0_DOMAIN + '/'
-                )
-
-                return payload
-
-            except jwt.ExpiredSignatureError:
-                raise AuthError({
-                    'code': 'token_expired',
-                    'description': 'Token expired.'
-                }, 401)
-
-            except jwt.JWTClaimsError:
-                raise AuthError({
-                    'code': 'invalid_claims',
-                    'description': 'Incorrect claims. Please, check the audience and issuer.'
-                }, 401)
-            except Exception:
-                raise AuthError({
-                    'code': 'invalid_header',
-                    'description': 'Unable to parse authentication token.'
-                }, 400)
-        raise AuthError({
-            'code': 'invalid_header',
-                    'description': 'Unable to find the appropriate key.'
-        }, 400)
-        # ____________________________________________________________________________________________________
 
     # ___________________________________________________AUTH___________________________________________________
 
@@ -193,10 +124,7 @@ def create_app(test_config=None):
                     new_account = Account(first_name=first_name,
                                           last_name=last_name, balance=init_balance)
                     new_account.insert()
-                    res_body['created'] = new_account.id
-                    res_body['first_name'] = new_account.first_name
-                    res_body['last_name'] = new_account.last_name
-                    res_body['balance'] = new_account.balance
+                    res_body = new_account.format()
                     res_body['success'] = True
 
                     return jsonify(res_body)
